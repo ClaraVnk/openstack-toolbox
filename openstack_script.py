fcc69ca2236b7e8079cd398b5c5fecbb9c8e395f
claravnk <claravanacker27@gmail.com>
2025-05-18 22:31:25 +0200

    fix

--- openstack_script.py
+++ openstack_script.py
@@ -36,7 +36,8 @@ def get_billing_data_from_file(filepath):
     with open(filepath, 'r') as f:
         return json.load(f)
 
-def calculate_instance_cost(billing_data, instance_id=None, icu_to_chf=50, icu_to_euro=55.5):
+def calculate_instance_cost(billing_data, ls
+instance_id=None, icu_to_chf=50, icu_to_euro=55.5):
     if not billing_data:
         print(f"Aucune donnée de facturation disponible pour l'instance {instance_id}")
         return 0.0, 0.0
@@ -265,7 +266,7 @@ def main():
     #except ValueError:
         #print("Format invalide. Veuillez entrer la date au format YYYY-MM-DD HH:MM")
         #return
-    #billing_data = get_billing_data_from_file('billing.json')
+    billing_data = get_billing_data_from_file('billing.json')
     
     # Lister les ressources
     list_images(conn)
