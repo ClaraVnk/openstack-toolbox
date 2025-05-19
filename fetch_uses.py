ffb5ca70deae640036031ab99ea49b7595ae2d86
claravnk <claravanacker27@gmail.com>
2025-05-19 17:26:38 +0200

    fix

--- fetch_uses.py
+++ fetch_uses.py
@@ -61,10 +61,9 @@ def main():
 
         usages = defaultdict(lambda: {"cpu": 0, "ram": 0, "storage": 0})
 
-        for entry in data:
-            desc = entry.get("desc", {})
-            project_id = desc.get("project_id", "inconnu")
-            flavor = desc.get("flavor_name", "")
+        for entry in data.get("data", []):
+            project_id = entry.get("project_id", "inconnu")
+            flavor = entry.get("flavor_name", "")
             volume = float(entry.get("volume", 1.0))
 
             cpu, ram, disk = parse_flavor_name(flavor)
